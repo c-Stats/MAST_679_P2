dd24bf1061438727ba6263681f7a993655e62918
Francis Fregeau <frankfredj@gmail.com>
2022-10-23 22:54:21 -0400

    initial commit

---
+++ R_code.r
@@ -0,0 +1,5 @@
+path <- 'C:/Users/Frank/OneDrive/Documents/git/MAST_679_P2/data'
+
+play_by_play_data <- readRDS(paste(path, 'Play_by_play_Database.rds', sep = '/'))
+markov_data <- readRDS(paste(path, 'Markov_Database.rds', sep = '/'))
+
